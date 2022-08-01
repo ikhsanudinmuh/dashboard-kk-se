b6e33f949099e4a4f9524b6aafb6c7ae0244d206
Muhammad Ikhsanudin <muhikhsanudin1@gmail.com>
2022-08-01 10:22:49 +0700

    create unauthorized page

---
+++ resources/views/auth/unauthorized.blade.php
@@ -0,0 +1,10 @@
+@include('layouts.header')
+        <title>Unauthorized | Dashboard</title>    
+    </head>
+    <body>
+        <div class="container">
+            <h4>You are not allowed to access that page!</h4>
+            <a href="/">Back to home</a>
+        </div>
+    </body>
+</html>

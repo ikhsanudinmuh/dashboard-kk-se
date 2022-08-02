ce0aade859d5309856ddb5582dc24ef199b51615
Muhammad Ikhsanudin <muhikhsanudin1@gmail.com>
2022-08-02 10:54:02 +0700

    create page for publication

---
+++ resources/views/publication/index.blade.php
@@ -0,0 +1,34 @@
+@include('layouts.header')
+        <title>Publications | Dashboard</title>    
+
+        <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.12.1/css/jquery.dataTables.css">  
+        <script type="text/javascript" charset="utf8" src="https://cdn.datatables.net/1.12.1/js/jquery.dataTables.js"></script>
+    </head>
+    <body>
+        @include('layouts.navbar')
+        <div class="container">
+            <div class="mb-3">
+                <table id="table_publication" class="display">
+                    <thead>
+                        <tr>
+                            <th>Column 1</th>
+                            <th>Column 2</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td>Row 1 Data 1</td>
+                            <td>Row 1 Data 2</td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+        </div>
+
+        <script>
+            $(document).ready( function () {
+                $('#table_publication').DataTable();
+            } );
+        </script>
+    </body>
+</html>
